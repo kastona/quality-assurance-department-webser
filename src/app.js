9ac6ed212e46597919df019042745410d2016628
kastona <stephenkastona@gmail.com>
2021-02-17 14:25:32 +0100

    fixes cors issues

--- src/app.js
+++ src/app.js
@@ -9,8 +9,27 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 let cors = require('cors')
-app.use(cors())
+//app.use(cors())
 
 const endpoint = process.env.END_POINT;
 const apiKey = process.env.KEY;
